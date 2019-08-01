7778a515eeb2939189f26324d08601d3285abf26
Shivi Kashyap <ibabpo@gmail.com>
2019-08-01 16:57:19 +0530

    generate token before try it

--- src/app/generatetoken/generatetoken.component.ts
+++ src/app/generatetoken/generatetoken.component.ts
@@ -8,6 +8,44 @@ import { ApiService } from '../api.service';
   styleUrls: ['./generatetoken.component.css']
 })
 export class GeneratetokenComponent implements OnInit {
+  tokenReq: any = [{
+    "app_name": "Demand Planning Tool",
+    "username": "skashyap",
+    "roles": [
+      "user"
+    ]
+  }];
+  tokenResp: any = [{
+    "success": true,
+    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfbmFtZSHI6IkRIVIlbWFuZCBQbGFubmluZyBUb29sIiwidXNlcm5hbWUiOiJza2FzaHlhcCIsInJv",
+    "expiry": 3600
+}];
+  invalidAppReq: any = [{
+    "app_name": "Demand Planning Toolx",
+    "username": "skashyap",
+    "roles": [
+      "user"
+    ]
+  }];
+  invalidAppResp: any = [{
+    "success": false,
+    "errorMessage": "Invalid App Name",
+    "errorCode": 104,
+    "app": "Demand Planning Toolx"
+  }];
+  invalidRolesReq: any = [{
+    "app_name": "Demand Planning Tool",
+    "username": "skashyap",
+    "roles": [
+      "Xadmin"
+    ]
+  }];
+  invalidRolesResp: any = [{
+    "success": false,
+    "errorMessage": "user roles are not valid.",
+    "errorCode": 107,
+    "app": "Demand Planning Tool"
+  }];
 
   constructor(public httpClient: HttpClient, public TryItModel: ApiService) { }
 
