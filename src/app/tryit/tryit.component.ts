97b7857dc1f32952555645dce4d7fe05d08cb7e8
Shivi Kashyap <ibabpo@gmail.com>
2019-08-07 13:59:04 +0530

    post call added

--- src/app/tryit/tryit.component.ts
+++ src/app/tryit/tryit.component.ts
@@ -2,16 +2,11 @@
 import { Subscription } from 'rxjs';
 import { DataService } from '../data.service';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AppEnvironment } from '../../environments/AppEnvironment';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  })
-};
+let headers = new HttpHeaders().set('Content-Type', 'application/json');
 
 @Component({
   selector: 'app-tryit',
@@ -26,6 +21,7 @@ export class TryitComponent implements OnInit {
   });
 
   // details: { "server": string, "endpoints": { [key: string]: any }, "method": { [key: string]: any } };
+  jsonReq: { "reqbody": { [key: string]: any }};
   apiDetails: Subscription;
   details: { "type": string, "route": string, "authentication": string };
   apiURL: any;
@@ -48,13 +44,11 @@ export class TryitComponent implements OnInit {
       + AppEnvironment.environment.server.url;
     this.method = this.details.type;
     this.endpoint = this.details.route;
-
   }
 
   getInfo() {
-    console.log(this.reqForm.value)
+    this.jsonReq = this.reqForm.value;
     var url = `${this.apiURL + this.endpoint}`;
-    console.log('url', url);
     switch (this.method) {
       case 'GET':
         this.httpClient.get(url).subscribe((data) => {
@@ -62,7 +56,7 @@ export class TryitComponent implements OnInit {
         });
         break;
       case 'POST':
-        this.httpClient.post(url, this.reqForm.value, httpOptions).subscribe((resp) => {
+        this.httpClient.post<any>(`${url}`, `${this.jsonReq.reqbody}`, { headers: headers }).subscribe((resp) => {
           this.response = resp;
         });
         break;
