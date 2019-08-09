240db95ef052279039952d5eaa446af4768cff32
Shivi Kashyap <ibabpo@gmail.com>
2019-08-09 12:23:38 +0530

    update mappings api added

--- src/app/updatemappings/updatemappings.component.ts
+++ src/app/updatemappings/updatemappings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
+import { AppEnvironment } from '../../environments/AppEnvironment';
 
 @Component({
   selector: 'app-updatemappings',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./updatemappings.component.css']
 })
 export class UpdatemappingsComponent implements OnInit {
+  mappingDetails: Subscription;
+  mappings: object;
+  response: any;
 
-  constructor() { }
+  constructor(private updateMappings: DataService) {
+  }
 
   ngOnInit() {
+    this.updateMappings.setData(AppEnvironment.environment.endpoints.updatemappings);
+    this.mappingDetails = this.updateMappings.getData().subscribe((map) => {
+      this.mappings = map;
+    });
   }
+  updateMappingReq: any = [
+    {
+      "refresh": true,
+      "add_data": true,
+      "index": "app_name",
+      "collection": "collection_name"
+    }
+  ];
+
+  updateMappingResp: any = [
+    {
+      "success": true,
+      "data": {
+        "SchemaMappingFound": true,
+        "IndexExists": true,
+        "DeletedIndex": true,
+        "CreateIndex": true,
+        "PushDataInProgress": true
+      }
+    }
+  ];
+
+  mappingerror: any = [
+    {
+      "success": false,
+      "errorMessage": "Schema mapping file not found.",
+      "errorCode": "137",
+      "app": "Demand Planning Tool"
+    }
+  ];
 
 }
