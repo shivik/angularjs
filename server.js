3e3238a0df5ed5f80de0e917fa07aef287dbce1e
Shivi Kashyap <ibabpo@gmail.com>
2019-08-12 13:07:04 +0530

    nodejs server added

---
+++ server.js
@@ -0,0 +1,49 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+var session = require('express-session');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/Elasticsearch')));
+
+//To be
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        path: "/",
+
+    }
+}));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/Elasticsearch/index.html'));
+});
+
+/**
+ * Get port and store in Express.
+ */
+const port = '9002';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API running on localhost:${port}`));
