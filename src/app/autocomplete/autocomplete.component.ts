581f83ceda2a58ecc33236102c0c8c39849d9a28
Shivi Kashyap <ibabpo@gmail.com>
2019-08-22 17:41:27 +0530

    recent search api

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
+import { AppEnvironment } from '../../environments/AppEnvironment';
 
 @Component({
   selector: 'app-autocomplete',
@@ -6,10 +9,113 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autocomplete.component.css']
 })
 export class AutocompleteComponent implements OnInit {
+  autocompleteSub: Subscription;
+  data: object;
+  response: any;
 
-  constructor() { }
+  constructor(private autocomplete: DataService) {
+  }
 
   ngOnInit() {
+    this.autocomplete.setData(AppEnvironment.environment.endpoints.autocomplete);
+    this.autocompleteSub = this.autocomplete.getData().subscribe((map) => {
+      this.data = map;
+    });
   }
+  autocompleteResp: any = [
+    {
+      "success": true,
+      "data": {
+        "took": 3,
+        "suggest": {
+          "suggestions": [
+            {
+              "text": "hari",
+              "offset": 0,
+              "length": 4,
+              "options": [
+                {
+                  "text": "Harikumar Thankappan Pillai",
+                  "_index": "employeesearch",
+                  "_type": "doc",
+                  "_id": "02001829",
+                  "_score": 1
+                },
+                {
+                  "text": "Haris Sirayev",
+                  "_index": "employeesearch",
+                  "_type": "doc",
+                  "_id": "02032545",
+                  "_score": 1
+                },
+                {
+                  "text": "Harichandra Kumar Yadav",
+                  "_index": "employeesearch",
+                  "_type": "doc",
+                  "_id": "02034528",
+                  "_score": 1
+                },
+                {
+                  "text": "Harinder Jeet Singh",
+                  "_index": "employeesearch",
+                  "_type": "doc",
+                  "_id": "02034629",
+                  "_score": 1
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  ];
+
+  autoError: any = [
+    {
+      "success": false,
+      "errorMessage": "Requested body is not valid. :[illegal_argument_exception] The required text option is missing",
+      "errorCode": 101,
+      "app": "PeopleFinder"
+    }
+  ];
+
+  autoMappings: any = [
+    {
+      "autocomplete": {
+        "properties": {
+          "all": {
+            "type": "completion"
+          },
+          "employeename": {
+            "type": "completion"
+          },
+          "emptype": {
+            "type": "completion"
+          },
+          "businessunit": {
+            "type": "completion"
+          },
+          "department": {
+            "type": "completion"
+          },
+          "empsuffix": {
+            "type": "completion"
+          },
+          "region": {
+            "type": "completion"
+          },
+          "totalexperience": {
+            "type": "completion"
+          }
+        }
+      }
+    }
+  ];
+
+  autocompletReq:any=[{
+    "index":"employeesearch",
+    "prefix": "hari",
+    "field": "employeename"
+  }];
 
 }
--- src/app/recentsearch/recentsearch.component.ts
+++ src/app/recentsearch/recentsearch.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
+import { AppEnvironment } from '../../environments/AppEnvironment';
 
 @Component({
   selector: 'app-recentsearch',
@@ -6,10 +9,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recentsearch.component.css']
 })
 export class RecentsearchComponent implements OnInit {
+  recentData: Subscription;
+  data: object;
+  response: any;
 
-  constructor() { }
+  constructor(private recent: DataService) {
+  }
 
   ngOnInit() {
+    this.recent.setData(AppEnvironment.environment.endpoints.recentsearches);
+    this.recentData = this.recent.getData().subscribe((map) => {
+      this.data = map;
+    });
   }
+  reqSample: any = [{
+    "size": 4,
+    "username": "skashyap",
+    "appname": "Global Lesson Learnt"
+  }];
+
+  savedData: any = [{
+    "username": "skashyap",
+    "appname": "Global Lesson Learnt",
+    "key": "assets",
+    "timecreated": 1557748509641,
+    "ipaddress": ":10.110.27.154",
+    "devicetype": "desktop"
+  }];
+
+  respSample: any = [{
+    "aggregations": {
+        "recent_searches": {
+            "doc_count_error_upper_bound": -1,
+            "sum_other_doc_count": 25,
+            "buckets": [
+                {
+                    "key": "permission",
+                    "doc_count": 1,
+                    "max_score": {
+                        "value": 1561713078761
+                    }
+                },
+                {
+                    "key": "improvement",
+                    "doc_count": 4,
+                    "max_score": {
+                        "value": 1561712782370
+                    }
+                },
+                {
+                    "key": "Cerenzie",
+                    "doc_count": 7,
+                    "max_score": {
+                        "value": 1561712578223
+                    }
+                },
+                {
+                    "key": "damaged",
+                    "doc_count": 2,
+                    "max_score": {
+                        "value": 1561635897550
+                    }
+                }
+            ]
+        }
+    }
+}];
+
+  autoMappings: any = [];
+
+  autocompletReq: any = [];
 
 }
